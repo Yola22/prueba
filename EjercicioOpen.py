c3a445ba68d8b2d978e64ac39f8d550509b54cf7
Yola22 <yyoplag@gmail.com>
2015-04-27 19:37:12 -0700

    estos son los arcivos bases de mi portafolio

---
+++ EjercicioOpen.py
@@ -0,0 +1,21 @@
+#texto = open("C:\\Users\\Yola\\Desktop\\PythonYolanda\\dani.txt")
+#print(texto.read())
+
+#Agenda = open("dani.txt",'w')
+#Agenda.truncate()
+#Agenda.write(" hola mundo, ya estoy escribiendo en Phyton")
+#Agenda.close()
+#agenda = open("dani.txt")
+
+#print (agenda)
+
+agenda = open("agenda.csv",'a')
+nombre = input("Introduce tu nombre de contacto > ")
+telefono = input("Introduce tu telefono > ")
+print("Seha guardado en la agenda el cotacto: ", nombre, "con el numero de telefono ",telefono)
+agenda.write(nombre)
+agenda.write(",")
+agenda.write(telefono)
+agenda.write(",")
+agenda.write("\n")
+agenda.close()
